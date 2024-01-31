d001906abb60a80ca7ed97cddd23fac0ccfc806f
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-01-31 13:49:43 +0100

    add photo to post schema

--- server/models/PostModel.js
+++ server/models/PostModel.js
@@ -5,8 +5,12 @@ const {Schema} = mongoose;
 const postSchema = new Schema({
     title: {type: String, required: true, trim: true},
     author: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    content: {type: String,  trim: true},        
+    content: {type: String,  trim: true},     
+    image: {type: String, }, 
+    imagePublicId: { type: String },  
 },
+
+      
 {timestamps: true}
 )
 
